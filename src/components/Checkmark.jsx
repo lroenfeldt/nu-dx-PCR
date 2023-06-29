faf970902b4d6a5690c2b5ce1066c13e6933d7a5
lanicet <anicetfosso@gmail.com>
2023-06-29 16:42:26 +0200

    checkmarks

--- src/components/Checkmark.jsx
+++ src/components/Checkmark.jsx
@@ -8,6 +8,23 @@ const Checkmark = ({ barcode, isNinetySix = false, style }) => {
       <div
         className={`checkmark`}
         style={{
+          backgroundColor: 'var(--green)',
+          ...style,
+        }}
+      >
+        <IoCheckmarkSharp
+          style={{
+            fontSize: 'smaller',
+          }}
+        />
+      </div>
+    );
+  }
+  if (barcode?.result != 'invalid' && barcode?.label === 'NTC') {
+    return (
+      <div
+        className={`checkmark`}
+        style={{
           backgroundColor: 'var(--red)',
           ...style,
         }}
@@ -38,15 +55,11 @@ const Checkmark = ({ barcode, isNinetySix = false, style }) => {
       <div
         className={`checkmark`}
         style={{
-          backgroundColor: 'var(--green)',
+          backgroundColor: 'var(--red)',
           ...style,
         }}
       >
-        <IoCheckmarkSharp
-          style={{
-            fontSize: 'smaller',
-          }}
-        />
+        <IoClose />
       </div>
     );
   }
