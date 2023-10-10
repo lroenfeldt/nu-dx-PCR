584aedbe1301465b5890501bc2c04463916d7b03
Tarek Jassine <tarekjassine@gmail.com>
2023-10-10 11:50:46 +0200

    edit features

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,50 +7,76 @@ import { TbMinusVertical } from "react-icons/tb";
 import moment from "moment";
 import "moment/dist/locale/de";
 import "moment/dist/locale/fr";
+import "moment/dist/locale/en-gb";
 
 const Footer = () => {
-	const { settings, dbConnection } = useData();
-	const navigate = useNavigate();
-	const { locale } = useTranslation();
-	const location = useLocation();
-	const [stateTime, setStateTime] = useState(moment().locale(locale).format("LL") + " " + moment().locale(locale).format("LTS"));
+  const { settings, dbConnection } = useData();
+  const navigate = useNavigate();
+  const { locale } = useTranslation();
+  const location = useLocation();
+  const [stateTime, setStateTime] = useState(
+    moment().locale(locale).format("LL") +
+      " " +
+      moment().locale(locale).format("hh:mm:ss")
+  );
 
-	const logo =
-		location.pathname === "/selectMethod" ? (
-			<div onDoubleClick={() => navigate("/debug")}>
-				<img src={nuDiagnostics} alt="" />
-			</div>
-		) : (
-			<span>{settings.account.data.name || "Procomcure Biotech"}</span>
-		);
+  const logo =
+    location.pathname === "/selectMethod" ? (
+      <div onDoubleClick={() => navigate("/debug")}>
+        <img src={nuDiagnostics} alt="" />
+      </div>
+    ) : (
+      <span>{settings.account.data.name || "Procomcure Biotech"}</span>
+    );
 
-	useEffect(() => {
-		const interval = setInterval(() => {
-			setStateTime(moment().locale(locale).format("LL") + " " + moment().locale(locale).format("LTS"));
-		}, 1000);
-		return () => clearInterval(interval);
-	}, [stateTime]);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setStateTime(
+        moment().locale(locale).format("LL") +
+          " " +
+          moment().locale(locale).format("hh:mm:ss")
+      );
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [stateTime]);
 
-	return (
-		<div className="Footer">
-			<div className="menu-left">{logo}</div>
-			<div className="menu-center">
-				<span></span>
-			</div>
-			<div className="menu-right">
-				<span>{stateTime}</span>
-				<div>
-					<TbMinusVertical color="#fff" size={40} />
-				</div>
-				<div>
-					<FaServer color="#fff" />
-				</div>
-				<div>
-					<FaCircle color={dbConnection ? "green" : "red"} />
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="Footer">
+      <div className="menu-left">{logo}</div>
+      <div className="menu-center">
+        <span></span>
+      </div>
+      <div className="menu-right">
+        {locale === "de" ? (
+          <span className="state-time-de">{stateTime}</span>
+        ) : (
+          ""
+        )}
+        {locale === "en" ? (
+          <div className="state-time-en">
+            <div>{stateTime}</div>
+            <div>PM</div>
+          </div>
+        ) : (
+          ""
+        )}
+        {locale === "fr" ? (
+          <span className="state-time-fr">{stateTime}</span>
+        ) : (
+          ""
+        )}
+        <div>
+          <TbMinusVertical color="#fff" size={40} />
+        </div>
+        <div>
+          <FaServer color="#fff" />
+        </div>
+        <div>
+          <FaCircle color={dbConnection ? "green" : "red"} />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
