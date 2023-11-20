e805d88785467ce53d8710fa9da18d0525d615e5
Tarek Jassine <tarekjassine@gmail.com>
2023-11-20 13:15:22 +0100

    modified with async await to clean code

--- electron/createFile.ts
+++ electron/createFile.ts
@@ -1,28 +1,24 @@
 import fs from "fs";
 import { app } from "electron";
 import path from "path";
-import moment from "moment";
-import "moment/dist/locale/de";
+import { format } from "date-fns";
 
-export const createFile: () => void = () => {
-  const fileName = `logs-${moment().format("DD-MM-YYYY")}-${new Date()
-    .getHours()
-    .toLocaleString("de-DE")}-${new Date()
-    .getMinutes()
-    .toLocaleString("de-DE")}.txt`;
+export const createFile = async (): Promise<void> => {
+  const currentDate = new Date();
+  const formattedDate = format(currentDate, "dd-MM-yyyy");
+  const hours = format(currentDate, "HH");
+  const minutes = format(currentDate, "mm");
+
+  const fileName = `${formattedDate}-${hours}-${minutes}.txt`;
   const filePath = path.resolve(app.getPath("userData"), "logs", fileName);
+
   const log = fs.promises.writeFile(filePath, "");
   const logsDir: string = path.resolve(app.getPath("userData"), "logs");
 
   try {
-    if (!fs.existsSync(logsDir)) {
-      fs.promises.mkdir(logsDir);
-    }
-
-    if (!log) {
-      log;
-    }
+    if (!fs.existsSync(logsDir)) await fs.promises.mkdir(logsDir);
+    if (!log) log;
   } catch (error) {
-    console.error(error);
+    console.log(`createFile.ts: ${error}`);
   }
 };
--- electron/deleteLogs.ts
+++ electron/deleteLogs.ts
@@ -3,22 +3,18 @@ import * as fs from "fs";
 import { app } from "electron";
 
 export const deleteLogs = () => {
-  const logsDir: string = path.resolve(app.getPath("userData"), "logs");
-
-  fs.readdir(logsDir, (_err, files) => {
-    files.forEach((file) => {
+  try {
+    const logsDir: string = path.resolve(app.getPath("userData"), "logs");
+    const files = fs.readdirSync(logsDir);
+    files.forEach(async (file) => {
       const filePath = path.join(logsDir, file);
-
-      fs.stat(filePath, (_err, stats) => {
-        const today = new Date();
-        const diffTime = Math.abs(today.getTime() - stats.birthtime.getTime());
-        const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
-
-        if (diffDays >= 30) {
-          const filePath = path.join(logsDir, file);
-          fs.promises.unlink(filePath);
-        }
-      });
+      const { birthtime } = await fs.promises.stat(filePath);
+      const today = new Date();
+      const diffTime = Math.abs(today.getTime() - birthtime.getTime());
+      const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+      if (diffDays >= 30) fs.promises.unlink(filePath);
     });
-  });
+  } catch (error) {
+    console.log(`delete.ts: ${error}`);
+  }
 };
